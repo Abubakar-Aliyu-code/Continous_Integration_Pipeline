5b42c17df37ecda117e8ba629af00e504c6d81fb
Abubakar-Aliyu-code <aliyuabubakar599@gmail.com>
2025-01-12 00:42:31 +0100

    updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.app import app
 
 def test_home():
