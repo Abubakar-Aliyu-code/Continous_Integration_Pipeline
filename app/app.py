022ed1748f047d9e4bda14c250910458d23436ad
Abu_Ali <abubakaraliyu599@gmail.com>
2025-01-17 15:35:52 +0100

    fixting..

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD!"
+    return "Hello, CI/CD Pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
