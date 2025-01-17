495cd355342bf497800dac70cc101826bc3340fb
Abu_Ali <abubakaraliyu599@gmail.com>
2025-01-17 17:34:17 +0100

    ggg

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello, CI/CD Pipeline!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    
-    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,11 @@ import sys
 import os
 
 # Add the root directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app.app import app
+
 
 def test_home():
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
     assert response.data == b"Hello, CI/CD Pipeline!"
