7acd55397674bf648219abcae7c258f6415ba2ed
Abu_Ali <abubakaraliyu599@gmail.com>
2025-01-17 16:02:25 +0100

    fixur

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ from app.app import app
 def test_home():
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b"Hello, CI/CD Pipeline!!"
+    assert response.data == b"Hello, CI/CD Pipeline"
